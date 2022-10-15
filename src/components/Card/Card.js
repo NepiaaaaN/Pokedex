ff25b00cc3a9820757465b076a6384a96f34af5e
NepiaaaaN <surface095@gmail.com>
2022-10-15 15:56:23 +0900

    ドキュメント整備

---
+++ src/components/Card/Card.js
@@ -0,0 +1,34 @@
+import React from 'react'
+import "./Card.css"
+
+const Card = ({ pokemon }) => {
+  return (
+    <div className='card'>
+      <div className="cardImg">
+        <img src={pokemon.sprites.front_default} alt="" />
+      </div>
+      <h3 className='cardName'>{pokemon.name}</h3>
+      <div className="cardTypes">
+        <div>タイプ</div>
+        {pokemon.types.map((type, index) => {
+          return <div key={index}>
+            <span className='typeName'>{type.type.name}</span>
+          </div>
+        })}
+      </div>
+      <div className="cardInfo">
+        <div className="cardData">
+          <p>重さ : {pokemon.weight}</p>
+        </div>
+        <div className="cardData">
+          <p>高さ : {pokemon.height}</p>
+        </div>
+        <div className="cardData">
+          <p>能力 : {pokemon.abilities[0].ability.name}</p>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Card
